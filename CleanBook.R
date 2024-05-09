9781650c5ddc498408c46e2a5a66004fbd736dd3
ThijmenWeijgertze <129500989+ThijmenWeijgertze@users.noreply.github.com>
2024-05-09 12:39:00 +0200

    changed cleanbook

--- CleanBook.R
+++ CleanBook.R
@@ -1,10 +1,8 @@
 # show files to be deleted
-print("Files to be deleted:")
 bookdown::clean_book()
 
 # ask before proceeding bc it's about deleting data
 confirmation <- readline(prompt = "Proceed (Y): ")
-print(user_input)
 
 if (confirmation == "Y") {
   # delete files
