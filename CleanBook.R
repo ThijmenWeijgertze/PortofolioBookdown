c7e63b26ddfef0b1bd6c865ff96243fa8b14ddab
ThijmenWeijgertze <129500989+ThijmenWeijgertze@users.noreply.github.com>
2024-05-09 13:17:08 +0200

    Finished CleanBook.R

--- CleanBook.R
+++ CleanBook.R
@@ -1,13 +1,29 @@
+#---- small script for cleaning up bookdown files ----#
+
+# executes with source("CleanBook.R") in the console
+# idea: might be a good function for the package
+
+# \033 starts ANSI
+# [ specicifies the start ANSI
+# 1 makeds it bold
+# ; seperates parameters
+# 31 makes it bold
+# m end ANSI
+# \033[0m same as above but the 0 species there to be no formatting anymore
+
 # show files to be deleted
 bookdown::clean_book()
 
-# ask before proceeding bc it's about deleting data
-confirmation <- readline(prompt = "Proceed (Y): ")
+# warning using ANSI
+cat("\033[1;31mWARNING: check the files before proceeding!\033[0m")
 
-if (confirmation == "Y") {
-  # delete files
+# ask before proceeding
+proceed <- readline(prompt = "Proceed deletion (Y): ")
+
+# if user types Y then delete files else dont delete files
+if (proceed == "Y") {
+  print("Files have been deleted")
   bookdown::clean_book(TRUE)
 } else {
-  # don't delete files
   print("Files have not been deleted")
 }
